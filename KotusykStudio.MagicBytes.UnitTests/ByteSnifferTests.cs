cf0f3997c7cc46bc6e093fc08856cbaa67bf0661
Denys Tersiantsev <gureyndormin@gmail.com>
2025-04-19 19:09:56 +0300

    Unit tests

---
+++ KotusykStudio.MagicBytes.UnitTests/ByteSnifferTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace KotusykStudio.MagicBytes.UnitTests;
+public class ByteSnifferTests
+{
+    [Fact]
+    public void GetFileExtension_ValidJpg_ReturnsJpg()
+    {
+        // Arrange
+        var fileBytes = new byte[] { 255, 216, 255, 224 }; // JPG magic number
+        // Act
+        var result = ByteSniffer.GetFileExtension(fileBytes);
+        // Assert
+        Assert.Equal(".jpg", result);
+    }
+
+    [Fact]
+    public void GetFileExtension_ValidBmp_ReturnsBmp()
+    {
+        // Arrange
+        var fileBytes = new byte[] { 66, 77 }; // BMP magic number
+        // Act
+        var result = ByteSniffer.GetFileExtension(fileBytes);
+        // Assert
+        Assert.Equal(".bmp", result);
+    }
+
+    [Fact]
+    public void GetFileExtension_UnknownFormat_ReturnsUnknown()
+    {
+        // Arrange
+        var fileBytes = new byte[] { 0x00, 0x00, 0x00 }; // Unknown format
+        // Act
+        var result = ByteSniffer.GetFileExtension(fileBytes);
+        // Assert
+        Assert.Equal(".unknown", result);
+    }
+}
