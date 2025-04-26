c1594f9deb090fa79736e8ab9edcb3c8da7186b2
Denys Tersiantsev <gureyndormin@gmail.com>
2025-04-26 11:04:36 +0300

    removing hardcoded strings and refactoring

--- KotusykStudio.MagicBytes/ByteSniffer.cs
+++ KotusykStudio.MagicBytes/ByteSniffer.cs
@@ -1,17 +1,21 @@
 ﻿using System.IO.Compression;
 
 namespace KotusykStudio.MagicBytes;
+
 public static class ByteSniffer
 {
+    private const string _relsFile = "_rels/.rels";
+    private const string _wordDocumentFile = "word/document.xml";
+    private const string _workbookFile = "xl/workbook.xml";
     public static string GetFileExtension(byte[] fileBytes)
     {
-        foreach (var magicNumber in MagicNumbersExtensions)
+        foreach (var magicNumber in _magicNumbersExtensions)
         {
             if (fileBytes.Length < magicNumber.Key.Length + magicNumber.Value.offset)
                 continue;
 
             var match = true;
-            for (int i = 0; i < magicNumber.Key.Length; i++)
+            for (var i = 0; i < magicNumber.Key.Length; i++)
             {
                 if (magicNumber.Key[i] != fileBytes[i + magicNumber.Value.offset])
                 {
@@ -21,20 +25,20 @@ public static class ByteSniffer
             }
             if (match)
             {
-                string extension = magicNumber.Value.extension;
-                if (extension == ".zip")
+                var extension = magicNumber.Value.extension;
+                if (extension == Extensions.Zip)
                 {
                     using var memoryStream = new MemoryStream(fileBytes);
                     using var zip = new ZipArchive(memoryStream);
-                    if (zip.Entries.Any(x => x.FullName.Contains("_rels/.rels")))
+                    if (zip.Entries.Any(x => x.FullName.Contains(_relsFile)))
                     {
-                        if (zip.Entries.Any(x => x.FullName.Contains("word/document.xml")))
+                        if (zip.Entries.Any(x => x.FullName.Contains(_wordDocumentFile)))
                         {
-                            extension = ".docx";
+                            extension = Extensions.Docx;
                         }
-                        else if (zip.Entries.Any(x => x.FullName.Contains("xl/workbook.xml")))
+                        else if (zip.Entries.Any(x => x.FullName.Contains(_workbookFile)))
                         {
-                            extension = ".xlsx";
+                            extension = Extensions.Xlsx;
                         }
                     }
                 }
@@ -42,39 +46,58 @@ public static class ByteSniffer
             }
         }
 
-        return ".unknown";
+        return Extensions.Unknown;
     }
 
-    public static readonly Dictionary<byte[], (string extension, int offset)> MagicNumbersExtensions = new()
+    private static readonly Dictionary<byte[], (string extension, int offset)> _magicNumbersExtensions = new()
     {
-        //картинки
-        { new byte[3] { 255, 216, 255 }, (".jpg", 0) },
-        { new byte[8] { 137, 80, 78, 71, 13, 10, 26, 10 }, (".png", 0) },
-        { new byte[6] { 71, 73, 70, 56, 55, 97 }, (".gif", 0) },
-        { new byte[6] { 71, 73, 70, 56, 57, 97 }, (".gif", 0) },
-        { new byte[2] { 66, 77 }, (".bmp", 0) },
-        { new byte[4] { 87, 69, 66, 80 }, (".webp", 8) },
-        //відео
-        { new byte[8] { 102, 116, 121, 112, 105, 115, 111, 109 }, (".mp4", 4) },
-        { new byte[4] { 0, 0, 1, 179 }, (".mpg", 0) },
-        { new byte[4] { 0, 0, 1, 186 }, (".mpg", 0) },
-        { new byte[4] { 65, 86, 73, 32 }, (".avi", 8) },
-        { new byte[16] { 48, 38, 178, 117, 142, 102, 207, 17, 166, 217, 0, 170, 0, 98, 206, 108 }, (".wmv", 0) },
-        { new byte[4] { 26, 69, 223, 163 }, (".webm", 0) },
-        //аудіо
-        { new byte[2] { 255, 251 }, (".mp3", 0) },
-        { new byte[2] { 255, 243 }, (".mp3", 0) },
-        { new byte[2] { 255, 242 }, (".mp3", 0) },
-        { new byte[3] { 73, 68, 51 }, (".mp3", 0) },
-        { new byte[4] { 87, 65, 86, 69 }, (".wav", 8) },
-        //документи
-        { new byte[5] { 37, 80, 68, 70, 45 }, (".pdf", 0) },
-        { new byte[8] { 65, 84, 38, 84, 70, 79, 82, 77 }, (".djvu", 0) },
-        { new byte[6] { 123, 92, 114, 116, 102, 49 }, (".rtf", 0) },
-        { new byte[4] { 80, 75, 3, 4 }, (".zip", 0) },
-        { new byte[8] { 208, 207, 17, 224, 161, 177, 26, 225 }, (".doc", 0) },
-        //файли
-        { new byte[6] { 82, 97, 114, 33, 26, 7 }, (".rar", 0) },
-        { new byte[6] { 55, 122, 188, 175, 39, 28}, (".7z", 0) }
+            #region Images
+
+            { new byte[3] { 255, 216, 255 }, (Extensions.Jpg, 0) },
+            { new byte[8] { 137, 80, 78, 71, 13, 10, 26, 10 }, (Extensions.Png, 0) },
+            { new byte[6] { 71, 73, 70, 56, 55, 97 }, (Extensions.Gif, 0) },
+            { new byte[6] { 71, 73, 70, 56, 57, 97 }, (Extensions.Gif, 0) },
+            { new byte[2] { 66, 77 }, (Extensions.Bmp, 0) },
+            { new byte[4] { 87, 69, 66, 80 }, (Extensions.Webp, 8) },
+
+            #endregion
+
+            #region Videos
+
+            { new byte[8] { 102, 116, 121, 112, 105, 115, 111, 109 }, (Extensions.Mp4, 4) },
+            { new byte[4] { 0, 0, 1, 179 }, (Extensions.Mpg, 0) },
+            { new byte[4] { 0, 0, 1, 186 }, (Extensions.Mpg, 0) },
+            { new byte[4] { 65, 86, 73, 32 }, (Extensions.Avi, 8) },
+            { new byte[16] { 48, 38, 178, 117, 142, 102, 207, 17, 166, 217, 0, 170, 0, 98, 206, 108 }, (Extensions.Wmv, 0) },
+            { new byte[4] { 26, 69, 223, 163 }, (Extensions.Webm, 0) },
+
+            #endregion
+
+            #region Audio
+
+            { new byte[2] { 255, 251 }, (Extensions.Mp3, 0) },
+            { new byte[2] { 255, 243 }, (Extensions.Mp3, 0) },
+            { new byte[2] { 255, 242 }, (Extensions.Mp3, 0) },
+            { new byte[3] { 73, 68, 51 }, (Extensions.Mp3, 0) },
+            { new byte[4] { 87, 65, 86, 69 }, (Extensions.Wav, 8) },
+
+            #endregion
+
+            #region Documents
+
+            { new byte[5] { 37, 80, 68, 70, 45 }, (Extensions.Pdf, 0) },
+            { new byte[8] { 65, 84, 38, 84, 70, 79, 82, 77 }, (Extensions.Djvu, 0) },
+            { new byte[6] { 123, 92, 114, 116, 102, 49 }, (Extensions.Rtf, 0) },
+            { new byte[8] { 208, 207, 17, 224, 161, 177, 26, 225 }, (Extensions.Doc, 0) },
+
+            #endregion
+
+            #region Archives
+
+            { new byte[4] { 80, 75, 3, 4 }, (Extensions.Zip, 0) },
+            { new byte[6] { 82, 97, 114, 33, 26, 7 }, (Extensions.Rar, 0) },
+            { new byte[6] { 55, 122, 188, 175, 39, 28}, (Extensions.SevenZ, 0) }
+
+            #endregion
     };
 }
---
+++ KotusykStudio.MagicBytes/Extensions.cs
@@ -0,0 +1,52 @@
+﻿namespace KotusykStudio.MagicBytes;
+
+public static class Extensions
+{
+    #region Images
+
+    public const string Jpg = ".jpg";
+    public const string Png = ".png";
+    public const string Gif = ".gif";
+    public const string Bmp = ".bmp";
+    public const string Webp = ".webp";
+
+    #endregion
+
+    #region Videos
+
+    public const string Mp4 = ".mp4";
+    public const string Mpg = ".mpg";
+    public const string Avi = ".avi";
+    public const string Wmv = ".wmv";
+    public const string Webm = ".webm";
+
+    #endregion
+
+    #region Audio
+
+    public const string Mp3 = ".mp3";
+    public const string Wav = ".wav";
+
+    #endregion
+
+    #region Documents
+
+    public const string Pdf = ".pdf";
+    public const string Djvu = ".djvu";
+    public const string Rtf = ".rtf";
+    public const string Doc = ".doc";
+    public const string Docx = ".docx";
+    public const string Xlsx = ".xlsx";
+
+    #endregion
+
+    #region Archives
+
+    public const string Zip = ".zip";
+    public const string Rar = ".rar";
+    public const string SevenZ = ".7z";
+
+    #endregion
+
+    public const string Unknown = ".unknown";
+}
